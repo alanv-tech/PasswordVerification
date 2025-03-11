0f9bed0d788bc009a91306b16aead26dcf5c76c2
alanv-tech <alanvu4769@gmail.com>
2025-03-11 15:31:20 -0700

    Update pwdmain.js

--- src/pwdmain.js
+++ src/pwdmain.js
@@ -1,9 +1,9 @@
 let correctpwd = "blob";
-let inputpwd = prompt("Please enter your name\n", "");
+let inputpwd = prompt("Please the password\n", "");
 if (inputpwd == null || inputpwd == "") {
   console.log("Please enter a password");
 } else if (inputpwd == correctpwd) {
-  alert("Correct");
+  alert("Correct password, please continue");
 } else {
-    alert("Incorrect");
+    alert("Incorrect password");
 }
