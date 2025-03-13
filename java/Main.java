4e236f8bd184fda6b0c9040a5b08f579e2b9ee1f
alanv-tech <alanvu4769@gmail.com>
2025-03-13 13:40:24 -0700

    Create Main.java

---
+++ java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Please enter the password");
+
+    Scanner scanner = new Scanner(System.in);
+    String inp = scanner.nextLine();
+
+    if (inp.equals("blob") {
+      System.out.println("Hi");
+    } else {
+        System.out.println("Hi");
+    }
+  }
+}
